9cba8fe57716323dece42e9ed66db74f3d667964
MateusDaviDev <mateusdavi.silva@hotmail.com>
2023-10-20 19:52:21 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LanchesMac.Models;
+
+
+namespace LanchesMac.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+        
+
+    }
+}
---
+++ Controllers/LancheController.cs
@@ -0,0 +1,24 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            this._lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewData["Titulo"] = "Todos os lanches teste";
+            var lanches = _lancheRepository.Lanches;
+            var totalLanches = lanches.Count();
+            ViewBag.Total = totalLanches;
+            return View(lanches);
+        }
+    }
+}
