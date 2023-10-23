2cddc576d8488a0407be250558beacc26111424e
MateusDaviDev <mateusdavi.silva@hotmail.com>
2023-10-22 23:28:24 -0300

    commit CarrinhoCompra

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -14,11 +15,16 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            ViewData["Titulo"] = "Todos os lanches teste";
-            var lanches = _lancheRepository.Lanches;
-            var totalLanches = lanches.Count();
-            ViewBag.Total = totalLanches;
-            return View(lanches);
+            //ViewData["Titulo"] = "Todos os lanches teste";
+            //var lanches = _lancheRepository.Lanches;
+            //var totalLanches = lanches.Count();
+            //ViewBag.Total = totalLanches;
+            //return View(lanches);
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "categoria atual";
+            ViewBag.Total = lanchesListViewModel.Lanches.Count();
+            return View(lanchesListViewModel);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -18,10 +18,16 @@ public class Startup
     {
         services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-        services.AddControllersWithViews();
+        
         services.AddTransient<ILancheRepository,LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        
+        services.AddControllersWithViews();
 
+        services.AddMemoryCache();
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +47,7 @@ public class Startup
         app.UseStaticFiles();
 
         app.UseRouting();
-
+        app.UseSession();   
         app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
